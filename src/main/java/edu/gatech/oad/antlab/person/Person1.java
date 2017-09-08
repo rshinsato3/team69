39cd8f63a35607a21f89aad5c09c6f75cc74aaa3
Squiids <rshinsato3@gatech.edu>
2017-09-08 02:57:33 -0400

    Adding updated Person1.java with a method asked for, not sure if runs correctly, but the file compiles.

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -5,8 +5,8 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
- *  @version 1.1
+ *  @author Rafael Shinsato
+ *  @version 1.2
  */
 public class Person1 {
   /** Holds the persons real name */
@@ -30,8 +30,25 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+          if (input.length < 2) {
+            return input;
+          }
+          else {
+	    List<Character> list = new ArrayList<Character>();
+            char[] arr= input.toCharArray();
+            for(char a: array) {
+              list.add(a);
+            }
+	    char firstChar = arr[0];
+            char secChar = arr[1];
+            arr[0] = arr[arr.length - 2];
+            arr[1] = arr[arr.length - 1];
+            for (int x = arr.length - 2 ; x > 2; x--) {
+                   arr[x] = arr[x - 2]; 
+                }
+		
+	    return arr.toString();
+          }
 	}
 	
 	/**
